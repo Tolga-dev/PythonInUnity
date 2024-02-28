c93ef9a752dc5d21263b477e624ee8828b4c1922
Tolga-dev <orhan.konat.12@gmail.com>
2024-02-28 10:06:29 +0300

    adding

--- Assets/Scripts/PythonRunnerTest.cs
+++ Assets/Scripts/PythonRunnerTest.cs
@@ -1,21 +1,15 @@
  
 using TMPro;
+using UnityEditor.Scripting.Python;
 using UnityEngine;
 
 public class PythonRunnerTest : MonoBehaviour
 {
     public TextMeshProUGUI output;
         
-    void Start()
-    {
-        
-      
-        
-    }
-        
     public void RunPython(string fileName)
     {
-        
+        PythonRunner.RunFile($"Assets/PythonScripts/{fileName}.py");
     }
         
 }
