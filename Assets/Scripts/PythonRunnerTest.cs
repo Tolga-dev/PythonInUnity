d66c709685897a7998066a0bd4f9c58cd101c0a6
Tolga-dev <orhan.konat.12@gmail.com>
2024-02-28 10:00:48 +0300

    fighting with packs

---
+++ Assets/Scripts/PythonRunnerTest.cs
@@ -0,0 +1,21 @@
+ 
+using TMPro;
+using UnityEngine;
+
+public class PythonRunnerTest : MonoBehaviour
+{
+    public TextMeshProUGUI output;
+        
+    void Start()
+    {
+        
+      
+        
+    }
+        
+    public void RunPython(string fileName)
+    {
+        
+    }
+        
+}
